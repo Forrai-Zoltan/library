45e577b10845a3f575e1cbb3fc90560b74202e3d
simon-forman <forrai.zoltan66@gmail.com>
2025-09-13 20:30:52 +0200

    layout first pass

---
+++ script.js
@@ -0,0 +1,6 @@
+// ICON
+const favicon = document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/x-icon";
+favicon.href = "/library/favicon.ico";
+document.head.appendChild(favicon);
